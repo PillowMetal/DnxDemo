e6b9ff7735fe9ebb506549180997586abe04a39b
Steve Swayne <steve@swaynesworld.net>
2015-08-28 14:56:14 -0600

    Added project and ignore files

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace DnxDemo
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Brought to you by DNX and no Visual Studio...");
+            Console.Read();
+        }
+    }
+}
